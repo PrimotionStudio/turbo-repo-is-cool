60ebae706d90e84a34895acff8e8875843ddbfc8
PrimotionStudio <oyedelenewton@gmail.com>
2026-07-26 19:02:21 +0100

    Turbo Repo is Cool

---
+++ packages/db/index.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const connectionString = process.env.DATABASE_URL!;
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString }),
+  });
+
+globalForPrisma.prisma = prisma;
+
+export * from "./generated/prisma/client";
